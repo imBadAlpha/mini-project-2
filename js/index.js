582426fae81a758e6fca8a4a225f9a423716176d
IamBadAlpha <romeonombrejr@gmail.com>
2022-10-27 12:23:21 +0800

    update on the login bug for home page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 function checkLogin() {
   if (
+    localStorage.getItem("usernam") == null &&
+    localStorage.getItem("passwor") == null
+  ) {
+    localStorage.setItem("usernam", "")
+    localStorage.setItem("passwor", "")
+  }
+
+  if (
     localStorage.getItem("usernam") != "" &&
     localStorage.getItem("passwor") != ""
   ) {
     console.log(localStorage.getItem("usernam"));
     window.location.replace("listing.html");
   }
+
 }
 
 function saveLocation() {
